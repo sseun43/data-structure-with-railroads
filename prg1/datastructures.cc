3305c09fe1264082fca9d00398f334cbf9dbcfd8
Akangbe Oluwaseun Samuel <sseun43@yahoo.com>
2024-04-05 18:40:19 +0300

    small fixes_8

--- prg1/datastructures.cc
+++ prg1/datastructures.cc
@@ -407,10 +407,14 @@ RegionID Datastructures::get_parent(RegionID id)
 std::vector<RegionID> Datastructures::station_in_regions(StationID id)
 {
     // Replace the line below with your implementation
+    Name stationName = get_station_name(id);
+    if(stationName == NO_NAME){
+        return {{NO_REGION}};
+    }
     auto search = map_of_station_region_id.find(id);
     if (search == map_of_station_region_id.end()) {
         // not found
-        return {{NO_REGION}};
+        return {{}};
     } else {
         // found
         // region should always be found
